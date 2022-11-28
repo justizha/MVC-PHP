f2e259308fad5f40cdf31272efcc2663cfadff12
justizha <alfathizhaaaaaaa@gmail.com>
2022-11-28 09:50:38 +0700

    Small Changes

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -3,7 +3,7 @@
   <h1 class="display-4">Welcome To MVC PHP </h1>
   <p class="lead">Hello my name is <?=$data['nama'];?>.</p>
   <hr class="my-4">
-  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-  <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+  <p>Object Oriented Programming Using MVC Model (Model,View,Controller)</p>
+  <a class="btn btn-primary btn-lg" href="https://www.php.net/docs.php" target="_blank" role="button">PHP Documentation</a>
 </div>
 </div>
